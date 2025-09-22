17fe74440ab09bec5d635df4fd4cf2a083b622b8
Seoyoung-Kyung <tjdud90352@gmail.com>
2025-09-22 11:01:24 +0900

    상품 도메인 파일 상태 커밋

---
+++ src/main/java/com/codepresso/codepresso/controller/product/ProductController.java
@@ -0,0 +1,80 @@
+package com.codepresso.codepresso.product.controller;
+
+import com.codepresso.codepresso.dto.product.ProductDetailResponse;
+import com.codepresso.codepresso.dto.product.ProductListResponse;
+import com.codepresso.codepresso.dto.product.ReviewListResponse;
+import com.codepresso.codepresso.entity.product.Review;
+import com.codepresso.codepresso.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    // 1. 상품 전체 조회
+//    @GetMapping
+//    public String getProducts(@RequestParam(required = false) String category, Model model) {
+//
+//        if (category == null || category.isEmpty()) {
+//            return "redirect:/products?category=COFFEE";
+//        }
+//
+//        List<ProductListResponse> products;
+//
+//        // 카테고리별 상품 조회
+//        products = productService.findProductsByCategory(category);
+//
+//        model.addAttribute("products", products);
+//        model.addAttribute("currentCategory", category);
+//
+//        return "product";
+//    }
+
+    @GetMapping
+    public List<ProductListResponse> getProducts(@RequestParam(required = false, defaultValue = "COFFEE") String category) {
+        // 카테고리별 상품 조회
+        return productService.findProductsByCategory(category);
+    }
+
+//    // 2. 상품 상세 조회
+//    @GetMapping("/{productId}")
+//    public String getProduct(@PathVariable Long productId, Model model) {
+//        ProductDetailResponse pdResponse = productService.findByProductId(productId);
+//        model.addAttribute("productDetail", pdResponse);
+//        return "productDetail";
+//    }
+
+    // 팝업용 상품 상세 조회 (AJAX)
+//    @GetMapping("/{productId}/detail")
+//    @ResponseBody
+//    public ProductDetailResponse getProductDetail(@PathVariable Long productId) {
+//        return productService.findByProductId(productId);
+//    }
+
+    @GetMapping("/{productId}")
+    public ProductDetailResponse getProduct(@PathVariable Long productId) {
+        return productService.findByProductId(productId);
+    }
+
+
+    // 3. 상품 리뷰 목록
+    @GetMapping("/{productId}/reviews")
+    public List<ReviewListResponse> getProductReviews(@PathVariable Long productId) {
+        return productService.findProductReviews(productId);
+    }
+
+    // 4. 리뷰 작성
+    // @PutMapping("/{productId}/reviews")
+
+    // 5. 리뷰 수정
+    // @PutMapping("/{productId}/reviews/{reviewId}")
+
+    // 6. 리뷰 삭제
+    // @DeleteMapping("/{productId}/reviews/{reviewId}")
+}
---
+++ src/main/java/com/codepresso/codepresso/dto/product/NutritionInfoDTO.java
@@ -0,0 +1,37 @@
+package com.codepresso.codepresso.dto.product;
+
+import com.codepresso.codepresso.entity.product.NutritionInfo;
+import lombok.Getter;
+
+@Getter
+public class NutritionInfoDTO {
+    private double calories;
+
+    private double protein;
+
+    private double fat;
+
+    private double carbohydrate;
+
+    private double saturatedFat;
+
+    private double transFat;
+
+    private double sodium;
+
+    private double sugar;
+
+    private int cholesterol;
+
+    public NutritionInfoDTO(NutritionInfo nutritionInfo) {
+        this.calories = nutritionInfo.getCalories();
+        protein = nutritionInfo.getProtein();
+        fat = nutritionInfo.getFat();
+        carbohydrate = nutritionInfo.getCarbohydrate();
+        saturatedFat= nutritionInfo.getSaturatedFat();
+        transFat = nutritionInfo.getTransFat();
+        sodium = nutritionInfo.getSodium();
+        sugar = nutritionInfo.getSugar();
+        cholesterol = nutritionInfo.getCholesterol();
+    }
+}
