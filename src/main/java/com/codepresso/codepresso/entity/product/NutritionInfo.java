0909dc8bd1192b9cf6ae7c8f932b8fedbc2888c3
Seoyoung-Kyung <tjdud90352@gmail.com>
2025-09-25 19:54:23 +0900

    [Fix] NutritionInfo 엔티티 수정

--- src/main/java/com/codepresso/codepresso/entity/product/NutritionInfo.java
+++ src/main/java/com/codepresso/codepresso/entity/product/NutritionInfo.java
@@ -1,5 +1,6 @@
 package com.codepresso.codepresso.entity.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,6 +19,7 @@ public class NutritionInfo {
     @OneToOne
     @MapsId // PK = FK
     @JoinColumn(name = "product_id")
+    @JsonIgnore
     private Product product;
 
     private double calories;
